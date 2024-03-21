7f01ae0c53ae459e4e0c8aef695804bb9e34b749
JuricLeo <ljurickc@gmail.com>
2024-03-21 17:10:31 +0100

    contact form finished

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -1,96 +1,156 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { toast } from "sonner";
+
+interface FormData {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+}
 
 const ContactPage = () => {
-  return ( 
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const [errors, setErrors] = useState<Partial<FormData>>({});
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    setErrors({ ...errors, [name]: "" });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let isValid = true;
+    const newErrors: Partial<FormData> = {};
+
+    if (formData.name.length < 8) {
+      newErrors.name = "Username must be at least 8 characters long";
+      isValid = false;
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(formData.email)) {
+      newErrors.email = "Please enter a valid email address";
+      isValid = false;
+    }
+
+    if (!formData.subject.trim()) {
+      newErrors.subject = "Please enter a subject";
+      isValid = false;
+    }
+
+    if (!formData.message.trim()) {
+      newErrors.message = "Please enter a message";
+      isValid = false;
+    }
+
+    if (isValid) {
+      console.log("Form data:", formData);
+      toast.success("Message sent successfully!");
+    } else {
+      setErrors(newErrors);
+      toast.error(
+        "There was an error while sending the message. Please try again."
+      );
+    }
+  };
+
+  return (
     <div>
-      <form className="space-y-8 pt-40">
-        <div className="flex space-x-6">
-          <div className="flex flex-col flex-1 space-y-2">
-            <label htmlFor="name" className="text-white/30">
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Oli"
-              required
-              className="rounded-md bg-slate-800 p-6"
-            />
-          </div>
-          <div className="flex flex-col flex-1 space-y-2">
-            <label htmlFor="species" className="text-white/30">
-              Species
-            </label>
-            <input
-              type="text"
-              name="species"
-              placeholder="Dog"
-              required
-              className="rounded-md bg-slate-800 p-6"
-            />
-          </div>
+      <form onSubmit={handleSubmit} className="space-y-8 pt-40">
+        <div className="flex flex-col flex-1 space-y-2">
+          <label htmlFor="name" className="text-white/30">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="'Luka'"
+            className={cn(
+              "rounded-md bg-slate-800 p-6",
+              errors.name && "border-rose-500 border-2"
+            )}
+          />
+          {errors.name && <p className="text-red-500 text-sm">{errors.name}</p>}
         </div>
-        <div className="flex space-x-6">
-          <div className="flex flex-col flex-1 space-y-2">
-            <label htmlFor="age" className="text-white/30">
-              Age
-            </label>
-            <input
-              type="number"
-              name="age"
-              placeholder="10"
-              required
-              className="rounded-md bg-slate-800 p-6"
-            />
-          </div>
-          <div className="flex flex-col flex-1 space-y-2">
-            <label htmlFor="color" className="text-white/30">
-              Color
-            </label>
-            <input
-              type="text"
-              name="color"
-              placeholder="White"
-              required
-              className="rounded-md bg-slate-800 p-6"
-            />
-          </div>
+        <div className="flex flex-col flex-1 space-y-2">
+          <label htmlFor="email" className="text-white/30">
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="'moj@tvz.hr'"
+            className={cn(
+              "rounded-md bg-slate-800 p-6",
+              errors.email && "border-rose-500 border-2"
+            )}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email}</p>
+          )}
         </div>
-        <div className="flex space-x-6">
-          <div className="flex flex-col flex-1 space-y-2">
-            <label htmlFor="Food" className="text-white/30">
-              Favorite Food
-            </label>
-            <input
-              type="text"
-              name="favoriteFood"
-              placeholder="Sausage"
-              required
-              className="rounded-md bg-slate-800 p-6"
-            />
-          </div>
-          <div className="flex flex-col flex-1 space-y-2">
-            <label htmlFor="Activity" className="text-white/30">
-              Favorite Activity
-            </label>
-            <input
-              type="text"
-              name="favoriteActivity"
-              placeholder="Sleeping"
-              required
-              className="rounded-md bg-slate-800 p-6"
-            />
-          </div>
+        <div className="flex flex-col flex-1 space-y-2">
+          <label htmlFor="subject" className="text-white/30">
+            Subject
+          </label>
+          <input
+            type="text"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            placeholder="'Pets Table'"
+            className={cn(
+              "rounded-md bg-slate-800 p-6",
+              errors.subject && "border-rose-500 border-2"
+            )}
+          />
+          {errors.subject && (
+            <p className="text-red-500 text-sm">{errors.subject}</p>
+          )}
+        </div>
+        <div className="flex flex-col flex-1 space-y-2">
+          <label htmlFor="message" className="text-white/30">
+            Message
+          </label>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="'Are you displaying all pets...'"
+            className={cn(
+              "rounded-md bg-slate-800 p-6 h-40 resize-none",
+              errors.message && "border-rose-500 border-2"
+            )}
+          />
+          {errors.message && (
+            <p className="text-red-500 text-sm">{errors.message}</p>
+          )}
         </div>
         <Button
           type="submit"
           className="flex ml-auto bg-sky-700 hover:bg-sky-600"
         >
-          Add
+          Submit
         </Button>
       </form>
     </div>
   );
-}
- 
+};
+
 export default ContactPage;
