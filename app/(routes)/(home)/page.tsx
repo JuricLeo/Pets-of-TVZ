e849134f2fe07778d59a33c88147dc504e34d192
JuricLeo <ljurickc@gmail.com>
2025-05-17 21:03:49 +0200

    make home page dynamic

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,6 +1,8 @@
 import Pets from "@/components/layout/home/pets";
 import Hero from "@/components/layout/home/hero";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <main>
