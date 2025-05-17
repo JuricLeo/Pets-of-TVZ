5dc57b7924ff3bff41c11c0b9967a259589d748e
JuricLeo <ljurickc@gmail.com>
2025-05-17 21:08:19 +0200

    build errors fix

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,8 +1,6 @@
 import Pets from "@/components/layout/home/pets";
 import Hero from "@/components/layout/home/hero";
 
-export const dynamic = "force-dynamic";
-
 export default function Home() {
   return (
     <main>
